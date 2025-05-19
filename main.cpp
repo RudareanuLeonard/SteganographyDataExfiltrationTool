800621e858fee58434c2f0f976f5f930deef4475
RudareanuLeonard <leonardrudareanu@gmail.com>
2025-05-19 21:16:07 +0300

    temp save

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "opencv2/imgcodecs.hpp"
 #include<opencv2/highgui/highgui.hpp>
 #include <typeinfo>
-
+#include <bitset>
 
 using namespace std;
 
@@ -19,6 +19,27 @@ void displayImage(cv::Mat image){
 }
 
 
+int modifyValue(int val){
+
+}
+
+void printRGBValues(cv::Mat image){
+
+    int i, j;
+
+    for(i = 0; i < image.size[0]; i ++)
+        for(j = 0; j < image.size[1]; j ++){
+            cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
+            uchar blue = pixel[0];
+            uchar green = pixel[1];
+            uchar red = pixel[2];
+            cout << "Pixel (" << i << ", " << j << "): B=" << (int)blue << ", G=" << (int)green << ", R=" << (int)red << endl;
+        }
+
+
+}
+
+
 int main(){
     if (fullWhiteImage.empty())
         cout << "Image can't be open by OpenCV\n";
@@ -28,8 +49,20 @@ int main(){
         // cout << fullWhiteImage.size;
     }
 
-    cv::imshow("qwertyu",fullWhiteImage);
-    cv::destroyWindow("Photoframe");//close the window and release allocate memory//
-    cv::waitKey(0);//wait till user press any key
+    
+    // printRGBValues(TwoColorsImage);
+
+
+    // cv::imshow("qwertyu",TwoColorsImage);
+    // cv::destroyWindow("Photoframe");//close the window and release allocate memory//
+    // cv::waitKey(0);//wait till user press any key
+
+    int a = 16;
+    bitset<8> b;
+
+    b = a;
+    cout << (b << 2);
+
+
 
 }
